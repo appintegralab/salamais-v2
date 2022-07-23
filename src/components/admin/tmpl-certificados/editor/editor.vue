2d12aede54dac8a72085b6c93afb59360c7a04fd
appintegralab <appintegra.lab@gmail.com>
2022-07-23 16:00:45 -0300

    testes editor

---
+++ src/components/admin/tmpl-certificados/editor/editor.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="border p-2 rounded">
+        oi
+        <mtext @showMenu="showMenu" />
+        <q-menu v-model="menu">
+            <div>
+                olá mundo
+            </div>
+        </q-menu>
+    </div>
+</template>
+
+<script>
+import mtext from "./text.vue"
+
+export default {
+    components: { mtext },
+    data() {
+        return {
+            menu: false
+        }
+    },   
+    methods: {
+        
+        showMenu(elem) {
+            console.log("showMenu",elem);
+        }
+    },
+
+}
+</script>
+<style lang="">
+    
+</style>
